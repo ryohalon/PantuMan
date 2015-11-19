9110f5f5dab63fb5cc79d31a2ae6157ab42e632a
floete <bemmannqiq@gmail.com>
2015-11-19 15:01:10 +0900

    シーンループの実装

---
+++ PantsuCollection/Assets/UI/FadeLooper.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeLooper : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private float fadeInTime = 1.0f;
+
+    [SerializeField]
+    private iTween.EaseType fadeInType = iTween.EaseType.linear;
+
+
+    Image thisImage = null;
+
+    void Awake()
+    {
+        thisImage = GetComponent<Image>();
+        if (thisImage == null)
+        {
+            Debug.Log("Image is null");
+        }
+
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash("from", 1.0f, "to", 0.3f, "time", fadeInTime, "easetype", fadeInType, "onupdate", "UpdateHandler","looptype",iTween.LoopType.pingPong));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateHandler(float value)
+    {
+        thisImage.color = new Color(thisImage.color.r, thisImage.color.g, thisImage.color.b, value);
+    }
+
+
+
+}
---
+++ PantsuCollection/Assets/Utility/GoGameMain.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoGameMain : MonoBehaviour {
+
+    [SerializeField]
+    float sceneFadeTime = 3.0f;
+
+    [SerializeField]
+    string nextSceneName = "GameMain";
+
+    bool alreadyChangedScene = false;
+
+	// Update is called once per frame
+	void Update () {
+        if (!alreadyChangedScene && Input.GetMouseButtonDown(0))
+        {
+            alreadyChangedScene = true;
+            StartCoroutine(LoadNextScene());
+        }
+
+
+	}
+
+    [SerializeField]
+    GameObject blackScreen = null;
+    
+    IEnumerator LoadNextScene()
+    {
+        var screen = GameObject.Find("Canvas").transform;
+        var clone = Instantiate(blackScreen);
+        clone.transform.SetParent(screen);
+
+        yield return new WaitForSeconds(sceneFadeTime);
+
+        Application.LoadLevel(nextSceneName);
+
+
+    }
+}
